a67834fcc7dcf33809658e0388be0aa82176a248
Mohammad Nourmohammadi <mohammad.nourmohammadi@sotoon.ir>
2023-01-05 22:15:56 +0330

    create shop views

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'shop'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product, Category
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'shop/index.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
+
+    def get_queryset(self):
+        return Category.objects.all()
+
+
+class ProductDetailView(generic.DetailView):
+    context_object_name = 'product'
+    model = Product
