62e266c83ea86c258a95dea62d5998d68c3ac4ba
Mohammad Nourmohammadi <mohammad.nourmohammadi@sotoon.ir>
2023-05-11 19:51:52 +0330

    init category page

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('product/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:slug>/', views.CategoryDetailView.as_view(), name='category_detail'),
 ]
