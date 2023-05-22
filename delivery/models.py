3efda69726ffa4f5abb9177e327b21f4efed310b
Mohammad Nourmohammadi <mohammad.nourmohammadi@sotoon.ir>
2023-05-22 19:10:04 +0330

    change refid to string

--- delivery/models.py
+++ delivery/models.py
@@ -17,7 +17,7 @@ class DeliveryPack(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='delivery_pack')
     created = models.DateTimeField(default=timezone.now)
     authority = models.CharField(max_length=100)
-    ref_id = models.IntegerField(default=0)
+    ref_id = models.CharField(max_length=30)
 
     class Meta:
         ordering = ('-created',)
